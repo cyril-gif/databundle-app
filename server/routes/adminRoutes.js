4057bf726168947de4d3a772313d6f549f2fcd39
Pascal Lantam Gbate <cyrillantam@gmail.com>
2026-08-02 16:25:53 +0000

    Add settings routes to adminRoutes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { checkWalletBalance, reconcileOrders } from "../controllers/adminController.js";
+import { checkWalletBalance, reconcileOrders, getSettings, updateSettings } from "../controllers/adminController.js";
 import { protect, admin } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/wallet-balance", protect, admin, checkWalletBalance);
 router.post("/reconcile-orders", protect, admin, reconcileOrders);
+router.get("/settings", protect, admin, getSettings);
+router.put("/settings", protect, admin, updateSettings);
 
 export default router;
