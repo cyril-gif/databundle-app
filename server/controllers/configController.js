376bde28b4a9f6c156c1ed07f50cd1084ece653c
Pascal Lantam Gbate <cyrillantam@gmail.com>
2026-08-04 14:55:19 -0700

    Refactor getPublicConfig to simplify response
    Removed Paystack public key and enabled status from response.

--- server/controllers/configController.js
+++ server/controllers/configController.js
@@ -6,20 +6,9 @@ import Settings from "../models/Settings.js";
 export const getPublicConfig = async (req, res, next) => {
   try {
     const settings = await Settings.getSingleton();
-    
-    // Get Paystack public key from environment
-    const paystackPublicKey = process.env.PAYSTACK_PUBLIC_KEY || null;
-    const paystackEnabled = !!paystackPublicKey;
-    
     res.json({
-      success: true,
-      data: {
-        paystackPublicKey: paystackPublicKey,
-        paystackEnabled: paystackEnabled,
-        checkerPrice: settings.checkerPrice,
-        frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3000',
-        websiteName: process.env.WEBSITE_NAME || 'AncestorDataHub'
-      }
+      paystackPublicKey: process.env.PAYSTACK_PUBLIC_KEY || null,
+      checkerPrice: settings.checkerPrice,
     });
   } catch (err) {
     next(err);
